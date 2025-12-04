a9b0b90b9de7e343806a237378ed57fb74b51ebd
Tom <antihackr@gmail.com>
2025-12-03 20:15:30 -0800

    added method to counter words in book

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         return f.read()
     
+def get_number_of_words(text):
+    words = text.split()
+    return len(words)
+
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text[:])
+    num_words = get_number_of_words(book_text)
+    print(f"Found {num_words} total words")
+    #print(book_text[:])
 
 main()
